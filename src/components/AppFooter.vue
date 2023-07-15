c020f6537e808a8e1e913a0e1d999a951fbbd7eb
Thibaut Goelff <thibaut.g@gmail.com>
2023-07-15 15:33:10 +0200

    Update AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,7 @@
   <div class="small text-center">
     <hr class="mt-5 mb-4" />
     <p class="text-muted">
-      Source for this webiste on <a href="https://github.com/Thib-G/tech-ethics-oath">GitHub</a>
+      Source for this website on <a href="https://github.com/Thib-G/tech-ethics-oath">GitHub</a>
     </p>
   </div>
 </template>
